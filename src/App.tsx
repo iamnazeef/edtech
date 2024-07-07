936750dd2b9c5aca884271fdf8cc12f570371800
iamnazeef <muhammednazeef6@gmail.com>
2024-07-07 23:07:25 +0530

    change layout to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { Layout } from 'antd'
+const { Header, Footer, Sider, Content } = Layout
+
 const App = () => {
   return (
-    <h1>Hello World</h1>
+    <Layout style={{ height: '100vh' }}>
+      <Sider width={100}>Sider</Sider>
+      <Layout>
+        <Header style={{ backgroundColor: "lightblue" }}>Header</Header>
+        <Content>
+          Hello World
+        </Content>
+        <Footer style={{ backgroundColor: "pink" }}>Footer</Footer>
+      </Layout>
+    </Layout>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Layout } from 'antd'
-const { Header, Footer, Sider, Content } = Layout
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Layout style={{ height: '100vh' }}>
-      <Sider width={100}>Sider</Sider>
-      <Layout>
-        <Header style={{ backgroundColor: "lightblue" }}>Header</Header>
-        <Content>
-          <App />
-        </Content>
-        <Footer style={{ backgroundColor: "pink" }}>Footer</Footer>
-      </Layout>
-    </Layout>
+    <App />
   </React.StrictMode>,
 )
